c78abad2da8ee4983e58942a5b783a324c7d37a4
Patrick McGee <patmcgee331@gmail.com>
2018-03-10 13:45:59 -0500

    Ran yardstick against lib

--- lib/json_rspec_match_maker/base.rb
+++ lib/json_rspec_match_maker/base.rb
@@ -1,6 +1,9 @@
-module JsonMatchers
+module JsonRspecMatchMaker
   # Error raised when the child class has failed to set @match_definition
+  # @api private
   class MatchDefinitionNotFound < StandardError
+    # Create an error message for a child class
+    # @param class_name [String] the name of the matcher class
     def initialize(class_name)
       super("Expected instance variable @match_defintion to be set for #{class_name}")
     end
@@ -8,25 +11,59 @@ module JsonMatchers
 
   # Base class that abstracts away all of the work of using the @match_definition
   class Base
-    attr_reader :instance, :json, :match_definition
+    # The object being expected against
+    # @api private
+    # @return [Object]
+    attr_reader :instance
 
+    # The json being tested
+    # @api private
+    # @return [Hash]
+    attr_reader :json
+
+    # Data structure that specifies how instance values relate to JSON values
+    # @api private
+    # @return [Hash]
+    attr_reader :match_definition
+
+    # Create a new JSON matcher
+    # @api public
+    # @param instance [Object] The object being serialized into JSON
+    # @example
+    #   JsonRspecMatchMaker.new(active_record_model)
+    #   JsonRspecMatchMaker.new(presenter_instance)
     def initialize(instance)
       @instance = instance
       @errors = {}
     end
 
+    # Match method called by RSpec
+    # @api public
+    # @return [Bool]
+    # @example
+    #   JsonRspecMatchMaker.new(user).matches?(user.to_json) #=> true
+    #   JsonRspecMatchMaker.new(dog).matches?(cat.to_json) #=> false
     def matches?(json)
       @json = json
       check_json_against_instance
       @errors.empty?
     end
 
+    # Error reporting method called by RSpec
+    # @api public
+    # @return [String]
+    # @example
+    #   match_maker.failure_message #=> 'Mismatch in field name: expected (Freddy) got (Harold)'
     def failure_message
       @errors.values.join('\n')
     end
 
     private
 
+    # Walks through the match definition, collecting errors for each field
+    # @api private
+    # @raise [MatchDefinitionNotFound] if child class does not set @match_definition
+    # @return [nil] returns nothing, adds to error list as side effect
     def check_json_against_instance
       raise MatchDefinitionNotFound, self.class.name unless @match_definition
       @match_definition.each do |error_key, match_def|
@@ -38,15 +75,22 @@ module JsonMatchers
       end
     end
 
+    # Iterates through an association of the instance while checking fields
+    # @api private
+    # @return [nil] returns nothing, adds to error list as side effect
     def check_association(error_key, association_definition)
-      association_definition[:attributes].each do |attr_error_key, match_functions|
-        association_definition[:association].call(instance).each_with_index do |associated_instance, idx|
+      association = association_definition[:association].call(instance)
+      association.each_with_index do |associated_instance, idx|
+        association_definition[:attributes].each do |attr_error_key, match_functions|
           full_error_key = "#{error_key}[#{idx}].#{attr_error_key}"
           check_values(full_error_key, match_functions, associated_instance, idx)
         end
       end
     end
 
+    # Checks fields on a single instance
+    # @api private
+    # @return [nil] returns nothing, adds to error list as side effect
     def check_values(error_key, match_functions, expected_instance = instance, idx = nil)
       instance_value = match_functions[:instance].call(expected_instance)
       json_value = if idx.nil?
@@ -57,8 +101,12 @@ module JsonMatchers
       add_error(error_key, instance_value, json_value) if instance_value != json_value
     end
 
+    # Adds an erorr to the list when a mismatch is detected
+    # @api private
+    # @return [String] the error message
     def add_error(field, instance_value, json_value)
-      @errors[field] = "Mismatch in field #{field}: expected (#{instance_value}), got: #{json_value}"
+      @errors[field] =
+        "Mismatch in field #{field}: expected (#{instance_value}), got: #{json_value}"
     end
   end
 end
