95161fb81a82e52a94c842c952d7ec7f4679aac4
Patrick McGee <patmcgee331@gmail.com>
2018-03-10 16:28:49 -0500

    Fixed all rubocop todos

--- lib/json_rspec_match_maker.rb
+++ lib/json_rspec_match_maker.rb
@@ -1,4 +1,6 @@
 require 'json_rspec_match_maker/version'
 require 'json_rspec_match_maker/base'
 
+# Main point of interest is that this module includes a utility
+# class for building JSON RSpec matchers
 module JsonRspecMatchMaker; end
